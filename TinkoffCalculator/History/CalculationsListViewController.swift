5520fa301fe183e6f8e68e10e88555e2e55953ef
Vera Kasianenko <112972833+CandyGoose@users.noreply.github.com>
2024-09-17 22:25:01 +0300

    TinkoffCalculator

---
+++ TinkoffCalculator/History/CalculationsListViewController.swift
@@ -0,0 +1,93 @@
+//
+//  CalculationsListViewController.swift
+//  TinkoffCalculator
+//
+
+import UIKit
+
+final class CalculationsListViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var calculations: [Calculation] = []
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+        initialize()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        initialize()
+    }
+    
+    private func initialize() {
+        modalPresentationStyle = .fullScreen
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.backgroundColor = .systemGray5
+        let tableHeaderView = UIView()
+        tableHeaderView.frame = CGRect(x: 0, y: 0, width: tableView.frame.size.width, height: 30)
+        tableView.tableHeaderView = tableHeaderView
+        tableView.tableFooterView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.size.width, height: 1))
+        
+        let nib = UINib(nibName: "HistoryTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "HistoryTableViewCell")
+        
+    }
+}
+
+extension CalculationsListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 90.0
+    }
+}
+
+extension CalculationsListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return calculations.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HistoryTableViewCell", for: indexPath) as! HistoryTableViewCell
+        let historyItem = calculations[indexPath.section]
+        
+        cell.configure(with: expressionToString(historyItem.expression), result: String(historyItem.result))
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd.MM.yyyy"
+        
+        return formatter.string(from: calculations[section].date as Date)
+    }
+}
+
+extension CalculationsListViewController {
+    private func expressionToString(_ expression: [CalculationHistoryItem]) -> String {
+        var result = ""
+        
+        for operand in expression {
+            switch operand {
+                case let .number(value):
+                    result += String(value) + " "
+                case let .operation(value):
+                    result += value.rawValue + " "
+            }
+        }
+        
+        return result
+    }
+}
